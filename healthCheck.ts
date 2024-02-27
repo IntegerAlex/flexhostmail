fe20116607d0e81195501b2b5b68a25f930e6b5d
akshat <akshat@localhost.localdomain>
2024-02-27 16:45:45 +0530

    added health cheching

---
+++ healthCheck.ts
@@ -0,0 +1,20 @@
+import axios from 'axios'; 
+
+const HEALTH_CHECK_URL = "http://localhost:9543/health"; 
+
+async function checkHealth() {
+  try {
+    const response = await axios.get(HEALTH_CHECK_URL);
+    if (response.status === 200 && response.data === 'OK') {
+      console.log("Health check successful!");
+    } else {
+      console.error("Health check failed!");
+      process.exit(1); 
+    }
+  } catch (error) {
+    console.error("Health check failed due to error:", error);
+    process.exit(1); 
+  }
+}
+
+checkHealth();
