a2da6c21c5129c0e07d8375bd474d0eddbea89c1
akshat <akshat@localhost.localdomain>
2024-02-27 22:01:13 +0530

    null

--- healthCheck.ts
+++ healthCheck.ts
@@ -1,20 +1,24 @@
-import axios from 'axios'; 
+import axios from 'axios';
 import { startFHM } from './index';
-const HEALTH_CHECK_URL = "http://localhost:9543/health"; 
+
+const HEALTH_CHECK_URL = "http://localhost:9543/health";
+
 startFHM();
-async function checkHealth(){
+
+async function checkHealth() {
   try {
-    
+    console.log("Attempting health check...");
     const response = await axios.get(HEALTH_CHECK_URL);
+
     if (response.status === 200 && response.data === 'OK') {
       console.log("Health check successful!");
     } else {
-      console.error("Health check failed!");
-      process.exit(1); 
+      console.error("Health check failed! Unexpected response:", response.data);
+      process.exit(1);
     }
   } catch (error) {
     console.error("Health check failed due to error:", error);
-    process.exit(1); 
+    process.exit(1);
   }
 }
 
